6780e784cea6c48fb66034c2c1f9728b71d40e2e
Sakib Islam <bachelorsakib@gmail.com>
2023-08-16 18:17:04 +0600

    check input

--- withdraw.js
+++ withdraw.js
@@ -6,10 +6,16 @@ document.getElementById('withdraw-btn').addEventListener('click', function(){
         alert("Please enter a valid number.");
         return;
     }
+       const preBalance = getTextFld('balance-total');
+    if (newWithdrawInput > preBalance) {
+        alert("Not enough money.");
+        return;
+    }
     const preWithdrawFld = getTextFld('withdraw-f');
     const newWithdrawBalance = preWithdrawFld + newWithdrawInput;
     setTextFld('withdraw-f', newWithdrawBalance);
-    const preBalance = getTextFld('balance-total');
+    // const preBalance = getTextFld('balance-total');
+
     const newBalance = preBalance - newWithdrawInput;
     setTextFld('balance-total', newBalance);
 })
